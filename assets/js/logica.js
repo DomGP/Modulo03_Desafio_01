60c11f6501d6498aec7461e09d2522ad87bf2166
Dominique Gajardo <dominique.gajardoap@gmail.com>
2024-02-01 18:03:01 -0300

    Cambiando variables var a let

--- assets/js/logica.js
+++ assets/js/logica.js
@@ -6,27 +6,27 @@ precioSpan.innerHTML = precio;
 
 //Botón Suma
 function botonSuma(){
-    var valorTotal = document.querySelector('.valor-total');
-    var sumaTotal = Number(valorTotal.innerHTML);
+    let valorTotal = document.querySelector('.valor-total');
+    let sumaTotal = Number(valorTotal.innerHTML);
     sumaTotal = sumaTotal+precio;
     valorTotal.innerHTML=sumaTotal
 
-    var valorCantidad = document.querySelector('.cantidad');
-    var cantidadPositiva = Number(valorCantidad.innerHTML);
+    let valorCantidad = document.querySelector('.cantidad');
+    let cantidadPositiva = Number(valorCantidad.innerHTML);
     cantidadPositiva = cantidadPositiva+1;
     valorCantidad.innerHTML=cantidadPositiva
 }
 
 //Botón Resta
 function botonResta(){
-    var valorTotal = document.querySelector('.valor-total');
-    var restaTotal = Number(valorTotal.innerHTML);
+    let valorTotal = document.querySelector('.valor-total');
+    let restaTotal = Number(valorTotal.innerHTML);
     restaTotal = restaTotal-precio;
     restaTotal = Math.max(0,restaTotal)
     valorTotal.innerHTML=restaTotal;
 
-    var valorCantidad = document.querySelector('.cantidad');
-    var cantidadNegativa = Number(valorCantidad.innerHTML);
+    let valorCantidad = document.querySelector('.cantidad');
+    let cantidadNegativa = Number(valorCantidad.innerHTML);
     cantidadNegativa = cantidadNegativa-1;
     cantidadNegativa = Math.max(0,cantidadNegativa)
     valorCantidad.innerHTML=cantidadNegativa;
